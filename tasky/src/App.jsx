7195dee05d1cd8a8cfcf036531463a73cbd15ddd
stephen1166 <20102455@mail.wit.ie>
2025-09-30 12:31:03 +0100

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -15,6 +15,11 @@ function App() {
     tasks[taskIndex].done = !tasks[taskIndex].done;
     setTaskState({tasks});
   }
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  }
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -27,6 +32,7 @@ function App() {
         priority={task.priority}
         done={task.done}
         markDone={() => doneHandler(index)}
+        deleteTask = {() => deleteHandler(index)}
         />
       ))} 
 
